a1e170279bab19a0fbc7bbd15f2784d915a89567
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-05-03 10:25:39 -0700

    updates

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
+import { initializeApp  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-4d068-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const dataRef = ref(database, "items")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shopList = document.getElementById("shopping-list")
+
+onValue(dataRef, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearList()
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+
+            appendItem(currentItem)
+        }
+    } else {
+        shopList.innerHTML = "No items to buy!"
+    }
+})
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
+
+    push(dataRef, inputValue)
+
+    clearInput()
+
+    console.log(`${inputValue} added to database`)
 })
+
+function clearInput() {
+    inputFieldEl.value = ""
+}
+
+function appendItem(x) {
+    let itemID = x[0]
+    let itemVal = x[1]
+    let newLi = document.createElement("li")
+
+    newLi.textContent = itemVal
+
+    newLi,addEventListener("click", function() {
+        let itemLocation = ref(database, `items/${itemID}`)
+        remove(itemLocation)
+    })
+
+    shopList.append(newLi)
+}
+
+function clearList() {
+    shopList.innerHTML = ""
+}
